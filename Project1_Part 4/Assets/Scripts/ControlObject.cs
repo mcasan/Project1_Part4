d2ccdd4365f2af7b48bcb34f153bae047a2b33f4
mcasan <31909324+mcasan@users.noreply.github.com>
2017-10-18 16:21:27 -0400

    Add it

---
+++ Project1_Part 4/Assets/Scripts/ControlObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// Here is to control time  & Print "it has been 3 seconds" 
+
+public class ControlObject : MonoBehaviour {
+
+	bool readyToPrint;
+
+	// Use this for initialization 
+	void Start () {
+
+		print ("The game has begun!");
+
+		readyToPrint = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time >= 3 && readyToPrint) {
+			
+			print ("It has been three seconds!");
+			readyToPrint = false;
+		
+		}
+		
+	}
+}
---
+++ Project1_Part 4/Assets/Scripts/Medals_and_Cubes.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medals_and_Cubes : MonoBehaviour {
+
+	int silver; 
+	int bronze;
+	int gold;
+	int miningSpeed;
+	int TimetoMine;
+	int bronzeSupply;
+	int silverSupply;
+	int Xposition;
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+
+	miningSpeed = 3;
+
+	silver = 0;
+
+	bronze = 0;
+	
+	bronzeSupply = 4;
+
+	silverSupply = 2;
+
+	TimetoMine = miningSpeed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time >= TimetoMine) {
+
+			TimetoMine = TimetoMine + miningSpeed;
+
+
+			if (bronzeSupply > 0) {
+
+				bronze = bronze + 1;
+
+				bronzeSupply = bronzeSupply - 1;
+
+				print (bronze + " bronze");
+
+
+				var mycube = Instantiate (cube, new Vector3 (Random.Range (-6, 9), Random.Range (-4, 4)), Quaternion.identity);
+
+				mycube.GetComponent<Renderer> ().material.color = Color.red;
+			} else if (bronzeSupply == 0) {
+
+				if (silverSupply > 0) {
+					
+					
+					silver = silver + 1;
+
+					silverSupply = silverSupply - 1;
+
+					print (silver + " silver");
+
+
+					var mycube = Instantiate (cube, new Vector3 (Random.Range (-6, 9), Random.Range (-4, 4)), Quaternion.identity);
+					mycube.GetComponent<Renderer> ().material.color = Color.white;
+				}
+			}
+
+			else if (bronze == 2 && silver == 2) {
+			
+				gold = gold + 1;		
+
+				print ( gold + " gold" );
+			
+			}
+
+		    }
+
+		
+				   }
+
+}
